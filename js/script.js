d315b31aa322e18d5f5e45b233d9ef5c5eae69c1
jmsrys <85496617+jmsrys@users.noreply.github.com>
2023-11-18 03:33:00 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,14 +6,6 @@ menubtn.onclick = () => {
     navbar.classList.toggle('active');
 }
 
-document.querySelector('#login-btn').onclick = () => {
-    document.querySelector('.login-form-container').classList.toggle('active');
-}
-
-document.querySelector('#close-login-btn').onclick = () => {
-    document.querySelector('.login-form-container').classList.remove('active');
-}
-
 
 window.onscroll = () => {
 
